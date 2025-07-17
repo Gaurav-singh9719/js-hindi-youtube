2eb699fabb6861bed864e1b196dfa43ad0b8909e
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-07-17 09:55:35 +0000

    Data Types Done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser, so no alert
+console.log(3
+    +
+    3)// code readability should be high
+console.log("Gaurav Singh"); 
+
+
+// Data Types in JavaScript
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+// Primitive Data Types
+// number => 2 to power 53
+// bigint
+// string => "Gaurav Singh"
+// boolean => true/false
+// null => empty value
+// undefined => variable is declared but not assigned
+// symbol => unique value
+
+
+// object
+connsole.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
