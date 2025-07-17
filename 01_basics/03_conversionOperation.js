4c1bfcc24613a8f624b74e515ce898fdc5df00be
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-07-17 15:58:36 +0000

    Operations Done.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,3 +20,31 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber); // string
 console.log(stringNumber); // "33"
+
+// ************************ Operations ********************** //
+
+let value = 3;
+let negvalue = -value; // -3
+console.log(negvalue); // -3
+
+let str1 = "Gaurav";
+let str2 = " Singh";
+let str3 = str1 + str2; // "Gaurav Singh"
+console.log(str3); // "Gaurav Singh"
+
+console.log("1" + 2); // "12"
+console.log(1 + "2"); // "12"
+console.log(1 + 2 + "3"); // "33"
+console.log("1" + 2 + 3); // "123"
+
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // 101
+
